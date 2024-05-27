2b9423def2e503ca4e4ca2262a359f4f3c2a8288
Arundhati Gadge <51483255+Arundhati1212@users.noreply.github.com>
2024-05-27 01:55:17 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,32 @@
+TaskMaster Pro
+As a dedicated frontend developer, I've been passionate about creating practical solutions that streamline project management processes. I'm thrilled to introduce my latest project, TaskMaster Pro, which showcases my skills in building intuitive and efficient web applications.
+
+Project Overview
+TaskMaster Pro: TaskMaster Pro is a comprehensive tool designed to simplify project management tasks. With this application, users can create, view, and delete projects, each containing a list of tasks associated with the project. Built using React.js, this manager offers a seamless and user-friendly interface for organizing and tracking project progress. Notably, TaskMaster Pro leverages browser storage to persist user data, ensuring that project and task information is retained even after reloading the page.
+
+Key Features and Technologies Utilized
+React.js: Leveraged React.js to develop a dynamic and responsive user interface, allowing users to easily create, view, and manage projects and tasks. In the process, I explored and implemented advanced React hooks such as useRef, forwardRef, and createPortal to enhance functionality and performance.
+State Management: Implemented efficient state management techniques within the React components, ensuring smooth and synchronized updates across the application.
+Data Organization: Utilized data structures and algorithms to organize projects and tasks, optimizing performance and user experience. Additionally, integrated browser storage to persist user data for a seamless experience across sessions.
+
+Why TaskMaster Pro Stands Out
+TaskMaster Pro is more than just a project; it's a testament to my commitment to creating practical, user-friendly solutions that enhance productivity. Through this project, I have refined my skills in React.js, state management, and data organization, gaining valuable experience in developing real-world applications that prioritize user experience and efficiency.
+
+How to Install and Run the Project
+To run MealMate locally, follow these steps:
+1.	Clone the repository: git clone https://github.com/Arundhati1212/TaskMaster-Pro.git
+2.	Navigate to the project directory: cd mealmate 
+3.	Install dependencies: npm install 
+4.	Start the development server: npm run dev 
+The application should now be running on http://localhost:3000.
+
+How to Use the Project
+1.	Create a Project:
+•	Navigate to the "Create Project" page and enter the project details.
+2.	Manage Tasks:
+•	Within each project, add tasks by entering task details and saving them. View and delete tasks as needed.
+3.	Data Persistence:
+•	The application uses browser storage to save your projects and tasks, so your data remains intact even after refreshing the page.
+
+Contributors
+•	Arundhati Gadge - linkedin.com/in/arundhati-gadge/
